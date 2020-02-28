e858b92bf69b83f8977cd3832e1ccc689024e17f
lalit <lalitpatil80@gmail.com>
2020-02-28 22:37:40 +0530

    routing added for each page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { Optimus2Component } from './optimus2/optimus2.component';
 import { Optimus3Component } from './optimus3/optimus3.component';
 import { Page2Component } from './page2/page2.component';
 import { Optimus4Component } from './optimus4/optimus4.component';
+import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+
+const appRoutes: Routes = [
+  { path: 'app-page1', component: Page1Component },
+  { path: 'app-page2', component: Page2Component },
+];
+
 
 
 @NgModule({
@@ -17,10 +25,15 @@ import { Optimus4Component } from './optimus4/optimus4.component';
     Optimus2Component,
     Optimus3Component,
     Page2Component,
-    Optimus4Component
+    Optimus4Component,
+    Page1Component
   ],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule ],
+ 
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule ,
+    RouterModule.forRoot(appRoutes)],
+
   providers: [],
-  bootstrap: [Page2Component]
+  // bootstrap: [Page2Component]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
